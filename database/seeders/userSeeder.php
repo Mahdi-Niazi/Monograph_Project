6985facbe3642a4e25279f93b1af80e5c2930939
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-05-15 23:08:19 +0430

    adding database files

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('users')->insert([
+            'empID'=>'1',
+            'username'=>'mahdi@gmail.com',
+            'password'=>Hash::make('mahdi1375')
+        ]);
+    }
+}
